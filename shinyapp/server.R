fe3803c538151538f01795c47e5ed197b50fd4fc
teserve <56771568+teserve@users.noreply.github.com>
2020-03-04 13:12:11 -0500

    Updating layout

--- shinyapp/server.R
+++ shinyapp/server.R
@@ -3,11 +3,16 @@ library(shiny)
 # Load data set
 library(tidyverse)
 data = read.csv("~/Github/sa-migration/Country Data/API_Download_DS2_en_csv_v2_792501.csv")
-colnames(data)= c("country", "indicator", c(1960:2007))
+colnames(data)= c("country", "indicator", c(1960:2019))
+
+data = data[, c(1, 2, 43:62)]
 
 countries = unique(data$country)
 indicators = unique(data$indicator)
-important = c("Population growth (annual %)", "Official exchange rate (LCU per US$, period average)", "Fertility rate, total (births per woman)", "Life expectancy at birth, total (years)", "GDP per capita (constant LCU)")
+important = c("Population growth (annual %)",
+              "Fertility rate, total (births per woman)",
+              "Life expectancy at birth, total (years)",
+              "GDP per capita (constant LCU)")
 
 total = data[0, ]
 
@@ -26,29 +31,17 @@ df = merge(df, ven_data, by = c("variable", 'indicator'))
 colnames(df) = c('year', 'indicator', 'country', 'val', 'VEN', 'valVEN')
 
 library(ggplot2)
-p = ggplot() +
-  geom_line(data = df, aes(x = year, y = val), color = 'blue') +
-  geom_line(data = df, aes(x = year, y = valVEN), color = 'red') +
-  facet_grid(cols = vars(country), rows = vars(indicator), scales = 'free') +
-  theme_bw()
 
 # Define server logic
 shinyServer(function(input,output) {
   
-  dataSubset = reactive({
-    df[union(which(df$country %in% input$country_selection), which(df$country %in% 'VEN')),]
-    })
-  
-  testPlot = reactive({
+  output$test = renderPlot({
     ggplot() +
-    geom_line(data = dataSubset, aes(x = year, y = val), color = 'blue') +
-    geom_line(data = dataSubset, aes(x = year, y = valVEN), color = 'red') +
-    facet_grid(cols = vars(country), rows = vars(indicator), scales = 'free') +
-    theme_bw()
+      geom_line(data = df[which(df$country %in% input$country_selection),], aes(x = year, y = val), color = 'blue') +
+      geom_line(data = df[union(which(df$country %in% input$country_selection), which(df$country %in% 'VEN')),], aes(x = year, y = valVEN), color = 'red') +
+      facet_grid(cols = vars(country), rows = vars(indicator), scales = 'free') +
+      theme_bw()
   })
-  
-  output$test = renderPlot({testPlot})
-  output$big_plot = renderPlot({p})
   
   output$map = renderImage({
     list(src = 'south_am.jpg')
--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -6,26 +6,49 @@ shinyUI(fluidPage(
   # Application title
   headerPanel('Migration Data'),
   
-  sidebarPanel(
-    selectInput('country_selection', 'Country:',
-                list('Argentina' = 'ARG',
-                     'Bolivia' = 'BOL',
-                     'Brazil' = 'BRA',
-                     'Chile' = 'CHL',
-                     'Colombia' = 'COL',
-                     'Ecuador' = 'ECU',
-                     'Guyana' = 'GUY',
-                     'Peru' = 'PER',
-                     'Paraguay' = 'PRY',
-                     'Surinae' = 'SUR',
-                     'Uruguay' = 'URY'
-                     )),
+  
+  fluidRow(
     
+    column(1,
+           radioButtons('country_selection', 'Country:',
+                        list('Argentina' = 'ARG',
+                             'Bolivia' = 'BOL',
+                             'Brazil' = 'BRA',
+                             'Chile' = 'CHL',
+                             'Colombia' = 'COL',
+                             'Ecuador' = 'ECU',
+                             'Guyana' = 'GUY',
+                             'Peru' = 'PER',
+                             'Paraguay' = 'PRY',
+                             'Surinae' = 'SUR',
+                             'Uruguay' = 'URY'
+                        ))
+           
+           
+           
+    ),
+    column(2, imageOutput('map', width = '100%', height = '100%'),
+    ),
+    column(3,
+           plotOutput('test'), width = 4, height = 200)
   ),
   
-  mainPanel(
-    imageOutput('map', width = '100%', height = '100%'),
-    plotOutput('big_plot'),
-    plotOutput('test')
-  )
+  # sidebarPanel(
+  #   selectInput('country_selection', 'Country:',
+  #               list('Argentina' = 'ARG',
+  #                    'Bolivia' = 'BOL',
+  #                    'Brazil' = 'BRA',
+  #                    'Chile' = 'CHL',
+  #                    'Colombia' = 'COL',
+  #                    'Ecuador' = 'ECU',
+  #                    'Guyana' = 'GUY',
+  #                    'Peru' = 'PER',
+  #                    'Paraguay' = 'PRY',
+  #                    'Surinae' = 'SUR',
+  #                    'Uruguay' = 'URY'
+  #                    )),
+  #   
+  # ),
+  
+  
 ))
